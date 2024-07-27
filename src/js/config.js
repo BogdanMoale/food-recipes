8ef728e6bc7ef570ef04746da088d72ec1c35437
Bogdan <moale.bogdan.ionica@gmail.com>
2024-07-27 12:51:04 +0300

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+//
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SECONDS = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '014f5bd1-69b0-42bd-8501-524354ad5e71';
+export const MODAL_CLOSE_SEC = 2.5;
